4379dfba324464f457a306f6cdbba57d477967cd
peter juul <peterjuul2@gmail.com>
2024-11-22 14:20:00 +0100

    saving untracked changes before switching branch

--- AST.java
+++ AST.java
@@ -16,7 +16,7 @@ public abstract class AST{
    expressions with And (Conjunction), Or (Disjunction), and Not
    (Negation). Moreover, an expression can be using any of the
    functions defined in the definitions. */
-// Task 1: 
+// Task 1:
 
 abstract class Expr extends AST{
 //We start by defining our eval method and for its subclasses
@@ -27,7 +27,7 @@ public abstract Boolean eval(Environment env);
 
 
 class Conjunction extends Expr{
-    // Example: Signal1 * Signal2 
+    // Example: Signal1 * Signal2
     Expr e1,e2;
     Conjunction(Expr e1,Expr e2){this.e1=e1; this.e2=e2;}
     // we override the method so its usage can be done in other classes.
@@ -39,7 +39,7 @@ class Conjunction extends Expr{
 }
 
 class Disjunction extends Expr{
-    // Example: Signal1 + Signal2 
+    // Example: Signal1 + Signal2
     Expr e1,e2;
     Disjunction(Expr e1,Expr e2){this.e1=e1; this.e2=e2;}
     // one or the other
@@ -62,8 +62,8 @@ class Negation extends Expr{
 
 class UseDef extends Expr{
     // Using any of the functions defined by "def"
-    // e.g. xor(Signal1,/Signal2) 
-    String f;  // the name of the function, e.g. "xor" 
+    // e.g. xor(Signal1,/Signal2)
+    String f;  // the name of the function, e.g. "xor"
     List<Expr> args;  // arguments, e.g. [Signal1, /Signal2]
     UseDef(String f, List<Expr> args){
 	this.f=f; this.args=args;
@@ -77,7 +77,7 @@ class UseDef extends Expr{
 }
 
 class Signal extends Expr{
-    String varname; // a signal is just identified by a name 
+    String varname; // a signal is just identified by a name
     Signal(String varname){this.varname=varname;}
     // Here we need the boolean to check if it contains the signal name or else return error
     @Override
@@ -105,7 +105,7 @@ class Def extends AST{
 // in the update section
 
 class Update extends AST{
-    // Example Signal1 = /Signal2 
+    // Example Signal1 = /Signal2
     String name;  // Signal being updated, e.g. "Signal1"
     Expr e;  // The value it receives, e.g., "/Signal2"
     Update(String name, Expr e){this.e=e; this.name=name;}
@@ -140,7 +140,7 @@ class Trace extends AST{
     public String toString(){
         // fixed compiling issue forgot to Initialize StringBuilder()
         StringBuilder sb = new StringBuilder();
-        // We append tµrue or false by 0 or 1 
+        // We append tµrue or false by 0 or 1
         for(Boolean value : values){
             sb.append(value ? "1": "0");
         }
@@ -153,12 +153,12 @@ class Trace extends AST{
 /* The main data structure of this simulator: the entire circuit with
    its inputs, outputs, latches, definitions and updates. Additionally
    for each input signal, it has a Trace as simulation input.
-   
+
    There are two variables that are not part of the abstract syntax
    and thus not initialized by the constructor (so far): simoutputs
    and simlength. It is suggested to use these two variables for
-   assignment 2 as follows: 
- 
+   assignment 2 as follows:
+
    1. all siminputs should have the same length (this is part of the
    checks that you should implement). set simlength to this length: it
    is the number of simulation cycles that the interpreter should run.
@@ -169,8 +169,8 @@ class Trace extends AST{
 */
 
 class Circuit extends AST{
-    String name;  
-    List<String> inputs; 
+    String name;
+    List<String> inputs;
     List<String> outputs;
     List<String>  latches;
     List<Def> definitions;
@@ -193,7 +193,7 @@ class Circuit extends AST{
 	this.updates=updates;
 	this.siminputs=siminputs;
     }
-    // Handling latches 
+    // Handling latches
     /*– Write a method latchesInit of class Circuit that takes an environment as argument and
         sets all latch outputs to value 0 in this environment*/
     public void latchesInit(Environment env){
@@ -264,7 +264,7 @@ System.out.println(env.toString());
 
         // The implemenation is similar to the initialization method.
         // But here we need to update the input signals for the cycle.
-        //Check if input value is same lentgh cycle.
+        // Check if input value is same lentgh cycle.
         for(Trace input : siminputs){
             if(cycle >= input.values.length){
                 error("Input for Trace signal: " + input.signal + " missing value for cycle " + cycle);
@@ -280,8 +280,9 @@ System.out.println(env.toString());
         for(Update update : updates){
             update.eval(env);
         }
-// Use the same toString to print the enviroment of the cycle
-System.out.println(env.toString());
+
+        // Use the same toString to print the enviroment of the cycle
+        System.out.println(env.toString());
 
 
       }
@@ -298,7 +299,7 @@ System.out.println(env.toString());
         for(int i = 1; i < simlength; i++){
             nextCycle(env,i);
         }
-         
+
       }
-    
+
 }
--- Environment.java
+++ Environment.java
@@ -8,16 +8,16 @@ class Environment {
     private HashMap<String,Boolean> variableValues = new HashMap<String,Boolean>();
 
     // Map for defined functions like "xor": for a string like "xor",
-    // you can look up its definition    
+    // you can look up its definition
     private HashMap<String,Def> defs;
 
     // Standard constructor that does not care about definitions
     // You cannot use getDef() below when using this constructor.
-    public Environment(){ this.defs=new HashMap<String,Def>();} 
+    public Environment(){ this.defs=new HashMap<String,Def>();}
 
     // Constructor that compute the map of function definitions, given
     // a the set of definitions as available in Circuit. You can then
-    // use getDef() below.    
+    // use getDef() below.
     public Environment(List<Def> listdefs) {
 	defs=new HashMap<String,Def>();
 	for(Def d:listdefs)
@@ -32,7 +32,7 @@ class Environment {
     // Lookup a definition, e.g., "xor"
     public Def getDef(String name){
 	Def d=defs.get(name);
-	if (d==null){ System.err.println("Function not defined: "+name); System.exit(-1); }  
+	if (d==null){ System.err.println("Function not defined: "+name); System.exit(-1); }
 	return d;
     }
 
@@ -41,28 +41,76 @@ class Environment {
     // the current environment and using it as an argument to the
     // constructor for the new environemnt
     public HashMap<String,Def> getDefs(){return defs;};
-    
+
     public void setVariable(String name, Boolean value) {
 	variableValues.put(name, value);
     }
-    
+
     public Boolean getVariable(String name){
-	Boolean value = variableValues.get(name); 
+	Boolean value = variableValues.get(name);
 	if (value == null) { System.err.println("Variable not defined: "+name); System.exit(-1); }
 	return value;
     }
 
     public Boolean hasVariable(String name){
-	Boolean v = variableValues.get(name); 
-	return (v != null);	
+	Boolean v = variableValues.get(name);
+	return (v != null);
     }
-    
+
     public String toString() {
 	String table = "";
 	for (Entry<String,Boolean> entry : variableValues.entrySet()) {
 	    table += entry.getKey() + "\t-> " + entry.getValue() + "\n";
 	}
 	return table;
-    }   
+    }
 }
+// updated UseDef class
+class UseDef extends Expr {
+    String f; // Function name
+    List<Expr> args; // List of argument expressions
 
+    public UseDef(String f, List<Expr> args) {
+        this.f = f;
+        this.args = args;
+    }
+
+    @Override
+    public Boolean eval(Environment env) {
+        // Step 1: Look up the function definition
+        Def def = env.getDef(f);
+
+        // Step 2: Evaluate the actual arguments in the current environment
+        List<Boolean> evaluatedArgs = new ArrayList<>();
+        for (Expr arg : args) {
+            evaluatedArgs.add(arg.eval(env));
+        }
+
+        // Step 3: Create a new environment for the function call
+        Environment newEnv = new Environment(env);
+        List<String> formalArgs = def.args;
+
+        if (formalArgs.size() != evaluatedArgs.size()) {
+            throw new IllegalArgumentException("Mismatched number of arguments for function: " + f);
+        }
+
+        for (int i = 0; i < formalArgs.size(); i++) {
+            newEnv.setVariable(formalArgs.get(i), evaluatedArgs.get(i));
+        }
+
+        // Step 4: Evaluate the function body in the new environment
+        return def.body.eval(newEnv);
+    }
+}
+// updated Def class
+class Def {
+    String f; // Function name
+    List<String> args; // Formal arguments
+    Expr body; // Function body (an expression)
+
+    public Def(String f, List<String> args, Expr body) {
+        this.f = f;
+        this.args = args;
+        this.body = body;
+    }
+}
